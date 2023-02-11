b536fb88358fd9ae5b249d7fe60ff28b884cb573
Daffa Izzuddin <rahmatullohdaffaizzuddin1@gmail.com>
2023-02-11 12:26:00 +0700

    add umami script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,16 @@
   import '../app.postcss';
 </script>
 
+<svelte:head>
+  <script
+    async
+    defer
+    data-website-id="b2ba4629-e586-4bdb-b4db-315e41fb412e"
+    src="https://umami.invasikode.com/umami.js"
+    data-domains="izzudd.invasikode.com"
+  ></script>
+</svelte:head>
+
 <!-- <Navbar /> -->
 <!-- <SectionTitle title="⚠️ under construction" reverse={true} class="warn text-white" /> -->
 <slot />
