3acbadc73a4950c95120797989e58a2bc67facc1
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2023-05-23 15:04:23 +0700

    update packages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,6 @@
     defer
     data-website-id="b2ba4629-e586-4bdb-b4db-315e41fb412e"
     src="https://umami.invasikode.com/umami.js"
-    data-domains="izzudd.invasikode.com"
   ></script>
 </svelte:head>
 
