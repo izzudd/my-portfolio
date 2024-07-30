1920bbdb00815708d19b09a04c57701a02acf901
izzudd <36723840+izzudd@users.noreply.github.com>
2024-07-30 19:31:00 +0700

    update projects

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -11,9 +11,9 @@
     <GridContainer>
       <div class="min-h-screen container mx-auto flex items-center justify-center py-8 px-6">
         <div class="grid gap-8 grid-cols-1 md:grid-cols-2 w-full">
-          {#each projects as { title, tag, link, description } (title)}
+          {#each projects as { title, tag, link, description, wip, stack } (title)}
             <a href={link} target="_blank">
-              <Card leading="# {tag.join(' | ')}" {title} {description} />
+              <Card leading="# {tag.join(' | ')}" {title} {description} mark="{wip && "WIP"}" icons="{stack}" />
             </a>
           {/each}
         </div>
